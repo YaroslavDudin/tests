22b1c1b643590563b82b73ccf62c3442c41875f7
komarik <yaroslavedudin@yandex.ru>
2024-04-17 12:55:15 +0300

    the end

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,14 @@
 import * as funcs from '../index.js';
-test("square", () => {
-    expect(funcs.square(2)).toBe(4);
-})
+import { describe, expect, it } from '@jest/globals';
+
+describe("task1", () => {
+    it('should work fine', () => {
+        expect(funcs.square(2)).toBe(4);
+    });
+});
+
+describe("task2", () => {
+    it('should capitalize the string', () => {
+        expect(funcs.capitalize('hello')).toBe('Hello');
+    });
+});
